753dc3adef82ecca25b23b78d4ef424ac1c57543
Graham Murray <graham.murr@yahoo.ie>
2017-03-01 23:38:30 +0000

    Extractor refactor

--- helper/extractor.js
+++ helper/extractor.js
@@ -6,19 +6,21 @@ var endpoints = {};
 var auth = {expire: '', token: ''};
 
 var Extractor = module.exports = function Extractor() {
-    obtainToken(function (err) {
-        if (err) {
-            console.log(err.message);
-        } else {
-            getEndpoints(function (err, result) {
-                if (err) {
-                    throw err;
-                }
-
-                endpoints = result;
-            });
-        }
-    });
+    obtainToken()
+        .then(() => {
+            getEndpoints()
+                .then(results => {
+                    endpoints = results
+                })
+                .catch(err => {
+                    console.warn('Failed to obtain API endpoints: ' + err.stack);
+                    process.exit();
+                });
+        })
+        .catch(error => {
+            console.warn('Couldn\'t obtain token: ' + error.message);
+            process.exit();
+        });
 };
 
 // Public methods
@@ -26,126 +28,133 @@ Extractor.prototype.launchLinkedin = function (props, next) {
     var req = getRequest(endpoints['run_linkedin'], 'POST');
     req.body = JSON.stringify(props);
 
-    performRequest(req, function (err, res) {
-        if (err) {
+    performRequest(req)
+        .then(result => {
+            if (result.jobs.length > 0) {
+                return next(null, result.jobs[0]);
+            } else {
+                var noJobError = new Error('Failed to trigger job');
+                return next(noJobError);
+            }
+        })
+        .catch(() => {
             var error = new Error('Oops something went wrong!');
             return next(error);
-        }
-
-        if (res.statusCode == 200) {
-            parseJSONResponse(res, function (err, result) {
-                if (err) return next(err);
-
-                if (result.jobs.length > 0) {
-                    return next(null, result.jobs[0]);
-                } else {
-                    var noJobError = new Error('Failed to trigger job');
-                    return next(noJobError);
-                }
-            });
-        }
-    });
+        });
 };
 
 Extractor.prototype.getJobsForUser = function (userId, count, next) {
-    var url = endpoints['job_by_user_id']+  userId + '?count=' + count;
+    var url = endpoints['job_by_user_id'] + userId + '?count=' + count;
     var req = getRequest(url, 'GET');
 
-    performRequest(req, function (err, res) {
-        if(err) {
-            var error = new Error('Oops something went wrong!');
+    performRequest(req)
+        .then(jobs => {
+            return next(null, jobs);
+        })
+        .catch(() => {
+            var error = new Error('Problem loading jobs');
             return next(error);
-        }
-
-        if (res.statusCode == 200) {
-            parseJSONResponse(res, function (err, jobs) {
-                if (err) return next(err);
-
-                return next(null, jobs);
-            });
-        } else {
-            var error = new Error('Problem loading jobs ' + res.statusCode);
-            return next(error);
-        }
-
-    });
+        });
 };
 
 
-
 // Private helpers
-function performRequest(req, callback) {
-    validateToken(function () {
-        console.log("Extractor request: " + req.url + " " + req.method);
-        req.headers.Authorization = auth.token;
+function performRequest(req) {
 
-        request(req, function (err, res, body) {
-            if (err) return callback(err);
+    return new Promise((resolve, reject) => {
+        validateToken()
+            .then(() => {
+                console.log("Extractor request: " + req.url + " " + req.method);
+                req.headers.Authorization = auth.token;
 
-            return callback(null, res)
-        });
+                request(req, function (err, res, body) {
+                    if (err) {
+                        reject(err);
+                    } else if (res.statusCode == 200) {
+                        parseJSONResponse(res)
+                            .then(obj => {
+                                resolve(obj);
+                            }).catch(error => {
+                            reject(error);
+                        });
+                    }
+
+                });
+            })
+            .catch(err => {
+                console.log('Request error: ' + JSON.stringify(req));
+                reject(err);
+            });
     });
 }
 
-function validateToken(next) {
+function validateToken() {
     var now = new Date();
 
-    if (auth.token == '' || now.getTime() > auth.expire.getTime()) {
-        obtainToken(function (err) {
-            if (err) {
-               console.log("Token refresh error: " + err);
-            }
-
-            return next();
-        });
-    }
-
-    return next();
+    return new Promise((resolve, reject) => {
+        if (auth.token == '' || now.getTime() >= auth.expire.getTime()) {
+            obtainToken()
+                .then(() => {
+                    resolve();
+                })
+                .catch(error => {
+                    reject(error);
+                });
+        } else {
+            resolve();
+        }
+    });
 }
 
-function obtainToken(next) {
+function obtainToken() {
     var req = getRequest(config.global.EXTRACTOR_TOKEN_URL, 'POST');
     req.body = JSON.stringify(config.global.EXTRACTOR_LOGIN);
 
-    request(req, function (err, res, body) {
-        if (err) {
-            console.log('Error obtaining token: ' + err);
-            return next(err);
-        }
+    return new Promise((resolve, reject) => {
+        request(req, function (err, res, body) {
+            if (err) {
+                reject(err);
+            } else if (res.statusCode == 200 && res.headers['content-type'] == 'application/json') {
 
-        if (res.statusCode == 200 && res.headers['content-type'] == 'application/json') {
+                var result = JSON.parse(body);
 
-            var result = JSON.parse(body);
+                if (result['token']) {
 
-            if (result['token']) {
+                    var expireTime = new Date();
+                    expireTime.setMinutes(expireTime.getMinutes() + 5);
 
-                var date = new Date();
-                date.setMinutes(date.getMinutes() + 25);
+                    auth.token = 'Bearer ' + result['token'];
+                    auth.expire = expireTime;
 
-                auth.token = 'Bearer ' + result['token'];
-                auth.expire = date;
-
-                console.log('Token obtained');
-                return next(null);
+                    console.log('Token obtained');
+                    resolve();
+                }
             }
-        }
+        });
     });
 }
 
-function getEndpoints(next) {
+function getEndpoints() {
     var req = getRequest('http://localhost:8000/api/v1/endpoints', 'GET');
 
-    performRequest(req, function (err, res) {
-        if (err) return next(err);
-
-        if (res.statusCode == 200) {
-            parseJSONResponse(res, function (err, obj) {
-                return next(null, obj);
+    return new Promise((resolve, reject) => {
+        performRequest(req)
+            .then(endpoints => {
+                resolve(endpoints);
+            })
+            .catch(err => {
+                reject(err);
             });
+    });
+}
 
+function parseJSONResponse(res) {
+    return new Promise((resolve, reject) => {
+        if (res.headers['content-type'] == 'application/json') {
+            resolve(JSON.parse(res.body));
         } else {
-            var error = new Error('Failed to fetch endpoints');
-            return next(error);
+            var err = new Error('Unable to parse response, body is not json')
+            reject(err);
         }
     });
 }
@@ -160,13 +169,4 @@ function getRequest(url, method) {
         method: method,
         body: ''
     }
-}
-
-function parseJSONResponse(res, next) {
-    if (res.headers['content-type'] == 'application/json') {
-        return next(null, JSON.parse(res.body));
-    }
-
-    var err = new Error('Unable to parse response, body is not json');
-    return next(err);
 }
