f11179ccffb3273be529fdfa7fd5a65b220e1ab3
Graham Murray <graham.murr@yahoo.ie>
2017-04-15 23:40:11 +0100

    Add marker

--- public/js/graph-render.js
+++ public/js/graph-render.js
@@ -50,6 +50,19 @@
         setGraphVisible(false);
 
         svg = d3.select(".graph").append("svg");
+        svg.append("svg:defs").selectAll("marker")
+            .data(["end"])      // Different link/path types can be defined here
+            .enter().append("svg:marker")    // This section adds in the arrows
+            .attr("id", String)
+            .attr("viewBox", "0 -5 10 10")
+            .attr("refX", 31)
+            .attr("refY", 0)
+            .attr("markerWidth", 6)
+            .attr("markerHeight", 6)
+            .attr("orient", "auto")
+            .append("svg:path")
+            .attr("d", "M0,-5L10,0L0,5");
+
         zoom = d3.behavior.zoom().scaleExtent([minZoom, maxZoom]);
         graph = svg.append("g");
         color = d3.scale.linear()
@@ -122,6 +135,7 @@
             .enter().append("line")
             .attr("class", "link")
             .style("stroke-width", nominalStroke)
+            .attr("marker-end", "url(#end)")
             .style("stroke", function (d) {
                 if (isNumber(d.score) && d.score >= 0) return color(d.score);
                 else return defaultLinkColor;
