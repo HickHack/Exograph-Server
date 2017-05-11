2fc6638bcbbb8ba7bd218c329b68d331594bf0e0
Graham Murray <graham.murr@yahoo.ie>
2017-05-11 21:39:06 +0100

    Search page

--- controller/search-controller.js
+++ controller/search-controller.js
@@ -1,3 +1,26 @@
 /**
  * Created by graham on 07/05/17.
  */
+
+exports.performSearch = function (req, res, next) {
+
+    if (req.query.q && req.query.q != '') {
+        var query = req.query.q.toLowerCase();
+
+        req.user.getNetworksByQuery(query, function (err, networks) {
+            if (err) {
+                res.status(500).send();
+            } else {
+                res.render('graph/search', {
+                    title: process.conf.app.NAME,
+                    pageName: 'Search results for "'+ query +'" ',
+                    user: req.user,
+                    networks: networks,
+                    jobs: []
+                });
+            }
+        });
+    } else {
+        res.redirect('/dashboard');
+    }
+};
